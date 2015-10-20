7e9d7fa803c68ea231a37117ed63cf545a1bd737
Alok Menghrajani <alok@squareup.com>
2015-10-20 12:39:22 -0700

    Add SecretDetailResponseV2Test

---
+++ api/src/test/java/keywhiz/api/automation/v2/SecretDetailResponseV2Test.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package keywhiz.api.automation.v2;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import java.time.OffsetDateTime;
+import org.junit.Test;
+
+import static keywhiz.testing.JsonHelpers.asJson;
+import static keywhiz.testing.JsonHelpers.jsonFixture;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SecretDetailResponseV2Test {
+  @Test public void serializesCorrectly() throws Exception {
+    SecretDetailResponseV2 secretDetailResponse = SecretDetailResponseV2.builder()
+        .name("secret-name")
+        .versions(ImmutableList.of("version1", "version2"))
+        .description("secret-description")
+        .content("YXNkZGFz")
+        .createdAtSeconds(OffsetDateTime.parse("2013-03-28T21:23:04.159Z").toEpochSecond())
+        .createdBy("creator-user")
+        .type("text/plain")
+        .metadata(ImmutableMap.of("owner", "root"))
+        .build();
+
+    assertThat(asJson(secretDetailResponse))
+        .isEqualTo(jsonFixture("fixtures/v2/secretDetailResponse.json"));
+  }
+}
