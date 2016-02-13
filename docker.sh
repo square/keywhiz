ae2fe4c8748821bba9efac12cb15c2681341773e
Cedric Staub <cs@css.bio>
2016-02-13 14:13:31 -0800

    Add MIGRATE_ON_STARTUP env option

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 RESET=$(tput -T xterm sgr0)
 RED=$(tput -T xterm setaf 1) 
@@ -11,6 +11,10 @@ if [ -z "$KEYWHIZ_CONFIG" ]; then
     echo "--- Use the KEYWHIZ_CONFIG environment variable to set a config file ---" 
     echo "------------------------------------------------------------------------"
     echo -n "${RESET}"
+fi
+
+if [ "$MIGRATE_ON_STARTUP" == "true" ]; then
+    java -jar server/target/keywhiz-server-*-SNAPSHOT-shaded.jar migrate $KEYWHIZ_CONFIG
 fi
 
 java -jar server/target/keywhiz-server-*-SNAPSHOT-shaded.jar $1 $KEYWHIZ_CONFIG
