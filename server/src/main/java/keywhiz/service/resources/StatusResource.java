32932fe5228797df33ca203b5a8e58c04cda7020
Matthew McPherrin <mmc@squareup.com>
2016-05-05 11:57:29 -0700

    Make /_status always return JSON

--- server/src/main/java/keywhiz/service/resources/StatusResource.java
+++ server/src/main/java/keywhiz/service/resources/StatusResource.java
@@ -1,9 +1,10 @@
 package keywhiz.service.resources;
 
-import com.codahale.metrics.annotation.Timed;
 import com.codahale.metrics.annotation.ExceptionMetered;
+import com.codahale.metrics.annotation.Timed;
 import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.HealthCheckRegistry;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.setup.Environment;
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +12,11 @@ import java.util.Map;
 import java.util.SortedMap;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.util.stream.Collectors.toList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
@@ -23,14 +26,27 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Produces(APPLICATION_JSON)
 public class StatusResource {
   private Environment environment;
+  private static final Logger logger = LoggerFactory.getLogger(SecretDeliveryResource.class);
 
   @Inject public StatusResource(Environment environment) {
     this.environment = environment;
   }
 
+  public class StatusResponse {
+    public String status;
+    public String message;
+    public SortedMap<String, HealthCheck.Result> results;
+
+    public StatusResponse(String status, String message, SortedMap<String, HealthCheck.Result> results) {
+      this.status = status;
+      this.message = message;
+      this.results = results;
+    }
+  }
+
   @Timed @ExceptionMetered
   @GET
-  public boolean get() {
+  public Response get() {
     HealthCheckRegistry checks = this.environment.healthChecks();
     SortedMap<String, HealthCheck.Result> results = checks.runHealthChecks();
 
@@ -40,10 +56,12 @@ public class StatusResource {
         .collect(toList());
 
     if (!failing.isEmpty()) {
-      throw new InternalServerErrorException(
-          "failing health checks: " + Arrays.toString(failing.toArray()));
+      logger.warn("Health checks failed: {}", results);
+      String message = "failing health checks: " + Arrays.toString(failing.toArray());
+      StatusResponse sr = new StatusResponse("critical", message, results);
+      return Response.serverError().entity(sr).build();
     }
-
-    return true;
+    StatusResponse sr = new StatusResponse("ok", "ok", results);
+    return Response.ok(sr).build();
   }
 }
