49dfb2f9629ca9a3498d7f0280e49033148cfc24
ruiwsquare <84882125+ruiwsquare@users.noreply.github.com>
2021-07-16 12:56:53 -0700

    Add instruction to help

--- cli/src/main/java/keywhiz/cli/configs/AddActionConfig.java
+++ cli/src/main/java/keywhiz/cli/configs/AddActionConfig.java
@@ -4,7 +4,9 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import java.util.List;
 
-@Parameters(commandDescription = "Add clients, groups, or secrets to KeyWhiz")
+@Parameters(commandDescription = "Add clients, groups, or secrets to KeyWhiz. When adding a secret, " +
+        "you will need to input the secret content if it is not already piped. " +
+        "Use Ctrl+D to signal end of the input.")
 public class AddActionConfig extends AddOrUpdateActionConfig {
   @Parameter(description = "<client|group|secret>")
   public List<String> addType;
