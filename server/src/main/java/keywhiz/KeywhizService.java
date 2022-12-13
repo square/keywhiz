b689234f68da3f4f01a08edfdaff208464803288
Michael Montgomery <mmontgomery@squareup.com>
2022-12-13 14:12:35 -0800

    Allowing Jackson ObjectMapper to serialize empty beans

--- server/src/main/java/keywhiz/KeywhizService.java
+++ server/src/main/java/keywhiz/KeywhizService.java
@@ -19,6 +19,7 @@ import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.annotations.VisibleForTesting;
@@ -209,6 +210,7 @@ public class KeywhizService extends Application<KeywhizConfig> {
     objectMapper.registerModules(new JavaTimeModule());
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
     return objectMapper;
   }
 
