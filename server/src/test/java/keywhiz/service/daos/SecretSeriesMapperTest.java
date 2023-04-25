e74eb74388aea78c5d94b5d79c7e7047304733f0
Michael Montgomery <77761476+mmontgomery-square@users.noreply.github.com>
2023-04-25 11:12:35 -0700

    Populating secret ownership via join rather than subquery (#1169)

--- server/src/test/java/keywhiz/service/daos/SecretSeriesMapperTest.java
+++ server/src/test/java/keywhiz/service/daos/SecretSeriesMapperTest.java
@@ -1,66 +1,27 @@
 package keywhiz.service.daos;
 
-import com.google.common.collect.ImmutableMap;
 import java.util.Random;
 import java.util.UUID;
 import javax.inject.Inject;
 import keywhiz.KeywhizTestRunner;
 import keywhiz.api.model.SecretSeries;
 import keywhiz.jooq.tables.records.SecretsRecord;
-import keywhiz.service.config.Readwrite;
-import org.jooq.DSLContext;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
 
 @RunWith(KeywhizTestRunner.class)
 public class SecretSeriesMapperTest {
-  @Inject private SecretSeriesMapper.SecretSeriesMapperFactory mapperFactory;
-  @Inject private DSLContext jooq;
-  @Inject @Readwrite private GroupDAO groupDAO;
-
-  private SecretSeriesMapper mapper;
-
-  @Before
-  public void before() {
-    mapper = mapperFactory.using(jooq);
-  }
+  @Inject private SecretSeriesMapper mapper;
 
   @Test
-  public void mapsRecordWithNullOwner() {
+  public void mapsRecord() {
     SecretsRecord record = minimalRecord();
     assertNull(record.getOwner());
 
     SecretSeries series = mapper.map(record);
     assertNull(series.owner());
-  }
-
-  @Test
-  public void mapsRecordWithExistingOwner() {
-    String ownerName = randomName();
-    Long ownerId = createGroup(ownerName);
-
-    SecretsRecord record = minimalRecord();
-    record.setOwner(ownerId);
-
-    SecretSeries series = mapper.map(record);
-    assertEquals(ownerName, series.owner());
-  }
-
-  @Test
-  public void mapsRecordWithNonExistingOwner() {
-    SecretsRecord record = minimalRecord();
-    record.setOwner(randomLong());
-
-    assertThrows(IllegalStateException.class, () -> mapper.map(record));
-  }
-
-  private Long createGroup(String ownerName) {
-    return groupDAO.createGroup(ownerName, null , null, ImmutableMap.of());
   }
 
   private SecretsRecord minimalRecord() {
